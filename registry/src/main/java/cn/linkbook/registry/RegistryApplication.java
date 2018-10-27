747704d80931c9326fd4a9dd35aae5eddae44eab
unknown <>
2018-10-28 00:16:12 +0800

    页面跳转配置

--- registry/src/main/java/cn/linkbook/registry/RegistryApplication.java
+++ registry/src/main/java/cn/linkbook/registry/RegistryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource(locations = {"classpath:/app.xml"})
+@ImportResource(locations = {"classpath*:/app.xml"})
 public class RegistryApplication {
 
     public static void main(String[] args) {
--- registry/src/main/java/cn/linkbook/registry/controller/PageController.java
+++ registry/src/main/java/cn/linkbook/registry/controller/PageController.java
@@ -1,16 +1,16 @@
 package cn.linkbook.registry.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author 王琦 <1124602935@qq.com>
  * @DATE 2018/10/27 21:55.
  * @Description
  */
-@RestController
+@Controller
 @RequestMapping("/to")
 public class PageController {
 
