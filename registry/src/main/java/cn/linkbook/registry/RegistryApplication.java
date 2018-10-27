8e8da2a73f81aa4cf7e12718333b8b8abbd59da2
xiao.liu <xiao.liu@tongdun.cn>
2018-10-28 00:39:29 +0800

    删除login-front模块

--- registry/src/main/java/cn/linkbook/registry/RegistryApplication.java
+++ registry/src/main/java/cn/linkbook/registry/RegistryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
-@ImportResource(locations = {"classpath:/app.xml"})
-public class RegistryApplication {
 
+@ImportResource(locations = {"classpath:/app.xml"})
+@SpringBootApplication
+public class RegistryApplication {
     public static void main(String[] args) {
         SpringApplication.run(RegistryApplication.class, args);
     }
