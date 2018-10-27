bc84e340448c20bc5639b43d846f686ebeded4df
unknown <>
2018-10-27 22:02:59 +0800

    数据源配置

---
+++ registry/src/main/java/cn/linkbook/registry/dal/inter/TestInfoDao.java
@@ -0,0 +1,14 @@
+package cn.linkbook.registry.dal.inter;
+
+import cn.linkbook.registry.dal.object.TestInfoDO;
+
+/**
+ * @author 王琦 <1124602935@qq.com>
+ * @DATE 2018/10/27 21:23.
+ * @Description
+ */
+public interface TestInfoDao {
+
+    boolean insert(TestInfoDO testInfoDO);
+
+}
---
+++ registry/src/main/java/cn/linkbook/registry/dal/object/BaseDO.java
@@ -0,0 +1,49 @@
+package cn.linkbook.registry.dal.object;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.util.StringUtils;
+
+import java.sql.Timestamp;
+
+/**
+ * @author 王琦 <1124602935@qq.com>
+ * @DATE 2018/10/27 21:23.
+ * @Description
+ */
+public class BaseDO {
+
+    private Integer id;
+    private String extraInfo;
+    private Timestamp createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getExtraInfo() {
+        return extraInfo;
+    }
+
+    public JSONObject getExtraInfoJson() {
+        if (StringUtils.isEmpty(extraInfo)){
+            return null;
+        }
+        return JSONObject.parseObject(extraInfo);
+    }
+
+    public void setExtraInfo(String extraInfo) {
+        this.extraInfo = extraInfo;
+    }
+
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+}
