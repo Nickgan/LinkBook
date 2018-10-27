7e6f542664b394dbf402158840a0552fe492ff56
unknown <>
2018-10-27 23:24:56 +0800

    修改mudule依赖

--- registry/src/main/java/cn/linkbook/registry/RegistryApplication.java
+++ registry/src/main/java/cn/linkbook/registry/RegistryApplication.java
@@ -2,8 +2,10 @@ package cn.linkbook.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(locations = {"classpath:/app.xml"})
 public class RegistryApplication {
 
     public static void main(String[] args) {
